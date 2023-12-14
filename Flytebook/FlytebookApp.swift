183c359555e40600d296be4d9ce44e85d95f6fd4
Richie Flores <rfloresc@icloud.com>
2023-12-14 12:42:24 -0800

    Initial Commit

---
+++ Flytebook/FlytebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlytebookApp.swift
+//  Flytebook
+//
+//  Created by Richie Flores on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FlytebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
