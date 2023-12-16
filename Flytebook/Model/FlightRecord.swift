341caae6d8eb679354b021be76516cf84de4da28
Richie Flores <rfloresc@icloud.com>
2023-12-15 16:26:19 -0800

    Add data model for flight record

---
+++ Flytebook/Model/FlightRecord.swift
@@ -0,0 +1,21 @@
+//
+//  FlightRecord.swift
+//  Flytebook
+//
+//  Created by Richie Flores on 12/15/23.
+//
+
+import Foundation
+
+struct LogbookEntry {
+    var flightDate: Date
+    var flightDuration: String
+    var aircraftType: String
+    var aircraftRegistration: String
+    var pilotInCommand: String
+    var coPilotOrInstructor: String
+    var departureAirport: String
+    var arrivalAirport: String
+    var flightPurpose: String
+    var remark: String
+}
