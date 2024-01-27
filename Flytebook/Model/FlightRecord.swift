97e6881c8e626ce51bf0ce25f0ee8d42bd547a7e
Richie Flores <rfloresc@icloud.com>
2024-01-26 23:00:13 -0800

    Refactor the data model and add Core Data Model file to the project.

--- Flytebook/Model/FlightRecord.swift
+++ Flytebook/Model/FlightRecord.swift
@@ -9,60 +9,41 @@ import Foundation
 
 struct FlightRecord {
     var id: UUID
-    var flightInfo: FlightInfo
-    var aircraftDetails: AircraftDetails
-    var flightRoute: FlightRoute
-    var aircraftCategory: AircraftCategory
-    var pilotingTime: PilotingTime
-    var flightConditions: FlightConditions
-    var landings: Landings
-    var instrumentApproaches: Int?
-    var additionalRemarks: String
-}
-
-// Flight Information
-struct FlightInfo {
+    
+    // Flight Information
     var dateOfFlight: Date
     var totalDuration: Double
-}
-
-// Aircraft Information
-struct AircraftDetails {
+    
+    // Aircraft Information
     var aircraftMake: String
     var aircraftModel: String
-    var aircraftIdentifier: String
-}
-
-// Flight Route
-struct FlightRoute {
-    var departure: String
-    var arrival: String
-}
-
-// Aircraft Category
-struct AircraftCategory {
+    var aircraftID: String
+    
+    // Flight Route
+    var airportDeparture: String
+    var airportArrival: String
+    
+    // Aircraft Category
     var airplaneSEL: Double?
     var airplaneMEL: Double?
     var groundTrainer: Double?
-}
-
-// Piloting Time
-struct PilotingTime {
-    var dualRecieved: Double?
+    
+    // Piloting Time
+    var dualReceived: Double?
     var pilotInCommand: Double?
-}
-
-// Flight Conditions
-struct FlightConditions {
-    var day: Double?
-    var night: Double?
-    var crossCountry: Double?
-    var actualInstrument: Double?
-    var simulatedInstrument: Double?
-}
-
-// Landings
-struct Landings {
-    var day: Int?
-    var night: Int?
+    
+    // Flight Conditions
+    var flightConditionDay: Double?
+    var flightConditionNight: Double?
+    var flightConditionCrossCountry: Double?
+    var flightConditionActualInstrument: Double?
+    var flightConditionSimulatedInstrument: Double?
+    
+    // Landings
+    var landingsDay: Int?
+    var landingsNight: Int?
+    
+    // Misc.
+    var instrumentApproaches: Int?
+    var additionalRemarks: String
 }
