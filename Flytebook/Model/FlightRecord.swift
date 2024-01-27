c3ef1453923834e0af7dc6f835d1215eef6bba7c
Richie Flores <rfloresc@icloud.com>
2024-01-26 19:46:47 -0800

    Modified Data Model to be more conclusive of actual log book

--- Flytebook/Model/FlightRecord.swift
+++ Flytebook/Model/FlightRecord.swift
@@ -8,14 +8,61 @@
 import Foundation
 
 struct FlightRecord {
-    var flightDate: Date
-    var flightDuration: String
-    var aircraftType: String
-    var aircraftRegistration: String
-    var pilotInCommand: String
-    var coPilotOrInstructor: String
-    var departureAirport: String
-    var arrivalAirport: String
-    var flightPurpose: String
-    var remark: String
+    var id: UUID
+    var flightInfo: FlightInfo
+    var aircraftDetails: AircraftDetails
+    var flightRoute: FlightRoute
+    var aircraftCategory: AircraftCategory
+    var pilotingTime: PilotingTime
+    var flightConditions: FlightConditions
+    var landings: Landings
+    var instrumentApproaches: Int?
+    var additionalRemarks: String
+}
+
+// Flight Information
+struct FlightInfo {
+    var dateOfFlight: Date
+    var totalDuration: Double
+}
+
+// Aircraft Information
+struct AircraftDetails {
+    var aircraftMake: String
+    var aircraftModel: String
+    var aircraftIdentifier: String
+}
+
+// Flight Route
+struct FlightRoute {
+    var departure: String
+    var arrival: String
+}
+
+// Aircraft Category
+struct AircraftCategory {
+    var airplaneSEL: Double?
+    var airplaneMEL: Double?
+    var groundTrainer: Double?
+}
+
+// Piloting Time
+struct PilotingTime {
+    var dualRecieved: Double?
+    var pilotInCommand: Double?
+}
+
+// Flight Conditions
+struct FlightConditions {
+    var day: Double?
+    var night: Double?
+    var crossCountry: Double?
+    var actualInstrument: Double?
+    var simulatedInstrument: Double?
+}
+
+// Landings
+struct Landings {
+    var day: Int?
+    var night: Int?
 }
